74ba24b681201c93725246692570d22f2dd31813
Nicovz21 <61903751+Nicovz21@users.noreply.github.com>
2021-04-12 17:20:16 -0400

    Added all images being used and xml for relax screen and main screen

--- app/src/main/java/com/hfad/workoutmaker/MainActivity.java
+++ app/src/main/java/com/hfad/workoutmaker/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //create object for both images
+        ImageView beginner_image = (ImageView) findViewById(R.id.BeginnerImage);
+        ImageView experienced_image = (ImageView) findViewById(R.id.ExperiencedImage);
+        //set on click listeners and establish what they do for both image buttons
+        //for experienced button
+        experienced_image.setOnClickListener(new ImageView.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                System.out.println("ExperiencedPressed");
+            }
+        });
+        //for beginner button
+        beginner_image.setOnClickListener(new ImageView.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                System.out.println("BeginnerPressed");
+            }
+        });
+
     }
+
 
 
 }
