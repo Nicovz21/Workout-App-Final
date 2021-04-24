ad502dd5dba5cc7676073a284e72a9a653487cc2
sspinner224 <sspinner@quinnipiac.edu>
2021-04-24 16:38:33 -0400

    new CardView implemented w/ data

---
+++ app/src/test/java/com/hfad/workoutmaker/MySQLiteHelperTest.java
@@ -0,0 +1,11 @@
+package com.hfad.workoutmaker;
+
+import static org.junit.Assert.*;
+
+public class MySQLiteHelperTest {
+
+    @org.junit.Before
+public void setUp() throws Exception {
+        
+  }
+}
