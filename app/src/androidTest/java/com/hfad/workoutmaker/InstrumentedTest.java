983b22b4e91f17acb28ce8d3d3f92d4232a5e37b
sspinner224 <sspinner@quinnipiac.edu>
2021-04-30 18:22:54 -0400

    instrumented test added

--- app/src/androidTest/java/com/hfad/workoutmaker/InstrumentedTest.java
+++ app/src/androidTest/java/com/hfad/workoutmaker/InstrumentedTest.java
@@ -1,64 +1,36 @@
-//package com.hfad.workoutmaker;
-//
-//import android.database.sqlite.SQLiteDatabase;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.junit.runners.JUnit4;
-//import java.util.List;
-//import android.util.Log;
-//import androidx.test.rule.ActivityTestRule;
-//import androidx.test.platform.app.InstrumentationRegistry;
-//import androidx.test.InstrumentationRegistry.getTargetContext;
-//
-//import static org.junit.Assert.*;
-//
-//@RunWith(JUnit4.class)
-//public class SQLiteInstrumentedTest {
-//    private SQLiteDatabase db;
-//    private WorkoutsDataSource dataSource;
-//
-//    //what to use instead of data source
-//
-//    private static String experience;
-//    private static String equipment;
-//    private static String workout;
-//
-//
-////    @Rule
-////    public ActivityTestRule<WorkoutActivity> mySQLiteHelperActivityTestRule = new ActivityTestRule<>(MySQLiteHelper.class);
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        getTargetContext().deleteDatabase(MySQLiteHelper.TABLE_EXERCISES);
-//        dataSource = new WorkoutsDataSource(getTargetContext());
-//        dataSource.open();
-//        db = dataSource.getDatabase();
-//    }
-//
-//    @Test
-//    public void insertData() {
-//
-//        //onView (some id) perform ____
-//        //onView (some id) check (matches (_____) )
-//    }
-//
-//    @Test
-//    public void getBeginnerAndNoEquipmentData() {
-//
-//        // from example
-//        dataSource.createWorkout("beginner","no weights","push-ups");
-//        List<Workout> wList = dataSource.getAllWorkouts();
-//        assertEquals(wList.size(),1);
-//        Log.i("test", String.valueOf(wList.get(0).getWorkout().equals("push-ups")));
-//
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        dataSource.close();
-//    }
-//}
+package com.hfad.workoutmaker;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
+import androidx.test.core.app.ActivityScenario;
+//import androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class InstrumentedTest {
+
+
+
+   ActivityScenario<RelaxActivity> relaxActivityScenario
+            =  ActivityScenario.launch(RelaxActivity.class);
+
+
+    @Test
+    public void nextScreen() {
+
+        onView(withId(R.id.one_day)).perform(click());
+   //     onView(withId(R.id.)).check(matches(isDisplayed()));
+
+   //     onView(withId(R.id.fragmentA)).check(//code here to check that specific text is displayed)
+
+    }
+
+
+}
